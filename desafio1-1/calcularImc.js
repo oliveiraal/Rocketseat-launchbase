ca49ef174552934c78347005ec0d80178a13493f
oliveiraal <alexandre.oliv.junior@gmail.com>
2021-02-13 11:50:37 -0300

    Update calcularImc.js

--- desafio1-1/calcularImc.js
+++ desafio1-1/calcularImc.js
@@ -1,1 +1,11 @@
+const nome = "Alexandre";
+const peso = 79;
+const altura = 1.73;
 
+const imc = Math.round(peso / (altura * altura));
+
+if (imc >= 30){
+	console.log(nome + ", você está acima do peso.")
+}else{
+	console.log(nome + ", você não está acima do peso")
+}
